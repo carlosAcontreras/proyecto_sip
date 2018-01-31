a97d2ff9ed3b4a7251f25a4849e939a15918cfc0
U-CONSTRUINTEGRAL\diego.contreras <diego.contreras@DIEGOC.CONSTRUINTEGRAL.LTDA>
2018-01-31 16:41:58 -0500

    se arregla el diseño de company se crea la funcion de permisos

--- src/app/admin/company/company.module.ts
+++ src/app/admin/company/company.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './company.component';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   imports: [
     CommonModule,
     CompanyRoutingModule,
-    HttpModule
+    HttpModule,
+    FormsModule
 
   ],
   declarations: [CompanyComponent]
